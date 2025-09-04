07e43b295e3bb15f50edbc5f242818fbcdcec2ce
ductv-dev <ductv@VietDuc-IT.>
2025-09-04 22:25:25 +0700

    fix

--- apps/client/src/app/authors/[id]/page.tsx
+++ apps/client/src/app/authors/[id]/page.tsx
@@ -2,7 +2,7 @@ import { SongByAuthors } from '@/containers/authors-page/song-by-authors/page';
 
 type PageProps = {
   params: { id: string };
-}
+};
 export default function SongByGenresPage({ params }: PageProps) {
   const decoded = decodeURIComponent(params.id);
 
